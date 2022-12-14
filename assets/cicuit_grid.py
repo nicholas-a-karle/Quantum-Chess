0624bcb2492c604f8d98a106c86ba7733b5276d7
Elrak01 <61857848+Elrak01@users.noreply.github.com>
2022-12-13 23:33:16 -0800

    first parts of display

--- assets/cicuit_grid.py
+++ assets/cicuit_grid.py
@@ -1,21 +1,34 @@
 import numpy as np
 from qiskit import *
-from globals import *
+from assets.globals import *
+import pygame
 
 class Gate:
-    def __init__(self, type, target = -1, control = -1):
+    def __init__(self, type = EMPTY, target = EMPTY, control = EMPTY):
         self.type = type
         self.target = target
         self.control = control
 
+    def clear(self):
+        self.type = EMPTY
+        self.target = EMPTY
+        self.control = EMPTY
 
 class CircuitGrid:
 
     def __init__(self, num_qbits, max_gates):
         self.num_qbits = num_qbits
         self.max_gates = max_gates
-        self.gates = [Gate(EMPTY)] * num_qbits * max_gates
+        self.gates = []
+        for i in range(num_qbits):
+            self.gates = self.gates + [Gate(EMPTY, i, EMPTY)]
 
+    #clear gates (set all to empty) and therefore circuit
+    def reset(self):
+        for gate in self.gates:
+            gate.clear()
+
+    #return a qiskit QuantumCircuit object which the CircuitGrid represents
     def construct_circuit(self):
         circ = QuantumCircuit(self.num_qbits)
         for gate in self.gates:
@@ -57,6 +70,11 @@ class CircuitGrid:
                 else:
                     circ.t(target)
             
+    def add_gate(self, type = EMPTY, target = EMPTY, control = EMPTY):
+        for gate in self.gates:
+            if gate.target == target:
+                gate.type = type
 
+#    def draw(self, surface):
 
     
--- qchess.py
+++ qchess.py
@@ -1,23 +1,37 @@
 import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((1200, 750))
-pygame.display.set_caption("Q Chess")
-clock = pygame.time.Clock()
+from assets.globals import *
+from assets.cicuit_grid import *
 
 def main():
 
+    pygame.init()
+    screen = pygame.display.set_mode((WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT))
+    pygame.display.set_caption(GAME_WINDOW_TITLE)
+    clock = pygame.time.Clock()
+
     exit = False
+    grid = CircuitGrid(GAME_MAX_QBITS, GAME_MAX_GATES)
 
     while not exit:
+        events = pygame.event.get()
+
         #exit if corner X button clicked
-        for event in pygame.event.get():
+        for event in events:
             if (event.type == pygame.QUIT): 
                 exit = True
 
+        screen.fill(OFF_WHITE)
+        pygame.draw.line(screen, BLACK, (0, QBIT_HEIGHT[0]), (WINDOW_SIZE_WIDTH, QBIT_HEIGHT[0]))
+        pygame.draw.line(screen, BLACK, (0, QBIT_HEIGHT[1]), (WINDOW_SIZE_WIDTH, QBIT_HEIGHT[1]))
 
+        pygame.display.flip()
+        pygame.display.update()
         #set framerate
-        clock.tick(60)
+        clock.tick(PRESET_FRAMERATE)
+
+
+
+    pygame.quit()
     
 if __name__ == '__main__':
     main()
